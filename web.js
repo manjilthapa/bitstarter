5eaee191081996a837ba7e9532220185f61d5b9d
manjilthapa <manjilthapa@gmail.com>
2013-08-16 12:17:51 +0000

    inserted readFile on web.js

--- web.js
+++ web.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
+app.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
