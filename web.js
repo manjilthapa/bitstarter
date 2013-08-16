ab98da8d9536d050d5fee598981b0c769a1ed37f
manjilthapa <manjilthapa@gmail.com>
2013-08-16 12:57:46 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,14 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
+var fs = require("fs");
+var buf = fs.readFileSync('index.html');
+var index = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(index);
 });
 
-app.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
